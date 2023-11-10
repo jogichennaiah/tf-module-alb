00e4dbbb92f1df3f42ad77914adb33a6f7883891
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-11 00:31:22 +0530

    Auto push

---
+++ sg.tf
@@ -0,0 +1,58 @@
+# Creates Security Group Public-ALB
+resource "aws_security_group" "allows-public" {
+  name        = "roboshop-${var.ENV}-public-alb-sg"
+  description = "Allows Only private traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  ingress {
+    description      = "Allows External HTTP Traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0./0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-alb-sg"
+  }
+}
+
+
+
+# Creates Security Group Private-ALB    
+resource "aws_security_group" "allows-private" {
+  name        = "roboshop-${var.ENV}-private-alb-sg"
+  description = "Allows Only private traffic"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  ingress {
+    description      = "Allows External HTTP Traffic"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["data.terraform_remote_state.vpc.outputs.VPC_CIDR", "data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-private-alb-sg"
+  }
+}
+
