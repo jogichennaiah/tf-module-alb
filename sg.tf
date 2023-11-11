2850c91b36a007cf2b6a5faf198d1cab68d833ba
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-11 16:13:14 +0530

    Auto push

--- sg.tf
+++ sg.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "alb_private" {
   ingress {
     description      = "Allows External HTTP Traffic"
     from_port        = 80
-    to_port          = 90
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
