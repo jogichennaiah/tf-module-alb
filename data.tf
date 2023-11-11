bb0a8b3111c6ddd5b6b52e502590bea9e83489ed
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-11 16:22:31 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket  = "b55-terraform-bucket"
-    key     = "alb/dev/terraform.tfstate"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
     region  = "us-east-1" 
   }
 }
