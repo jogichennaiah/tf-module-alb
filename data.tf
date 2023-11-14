2c21583382c9aeab2e2b8db607f636da9c9c06ca
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-15 00:35:11 +0530

    Auto push

--- data.tf
+++ data.tf
@@ -7,3 +7,25 @@ data "terraform_remote_state" "vpc" {
     region  = "us-east-1" 
   }
 }
+
+
+# Creates Listerner For the Private ALB
+
+# This creates the listener and adds to the private ALB
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
